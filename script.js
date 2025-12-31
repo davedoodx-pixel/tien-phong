2091f6f4d89f903ac620565be1363df031b34cd4
davedoodx-pixel <davedoodx@gmail.com>
2025-12-31 16:00:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -792,3 +792,6 @@ function bindCardUpdateZalo(cardEl, plate, priceText){
     setZaloOneTap(msg);
   });
 }
+const btt = document.getElementById("backToTop");
+window.addEventListener("scroll", ()=>{ btt.style.display = window.scrollY>300 ? "block":"none"; });
+btt?.addEventListener("click", ()=>window.scrollTo({top:0,behavior:"smooth"}));
